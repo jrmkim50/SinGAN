231ff2e890b630c4a602ab8790a805b3cb2cd71e
jrmkim50 <jrmkim50@gmail.com>
2023-07-16 19:44:11 -0700

    removed pacgan option because it does not work very well

--- config.py
+++ config.py
@@ -73,8 +73,6 @@ def get_arguments():
     parser.add_argument('--spectral_norm_g', action='store_true', help='spectral norm for generator', default=0)
     parser.add_argument('--spectral_norm_d', action='store_true', help='spectral norm for discriminator', default=0)
 
-    parser.add_argument('--packing_level', type=int, help='packing level from PacGAN', default=0)
-
     # parser.add_argument('--discrim_recon', action='store_true', help='include random recon image in discriminator loss', default=0) => DID NOT WORK
     # parser.add_argument('--min_ssim', action='store_true', help='use the minimum ssim value', default=0) => DID NOT WORK
     
